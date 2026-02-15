9ce265bb29d4a5d01a840d098dcda09e7584d44c
zuohl <10548200+zuohl@users.noreply.github.com>
2026-02-16 02:27:38 +0800

    更新 release-rule-set.sh

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -8,7 +8,7 @@ cd sing-geoip/rule-set
 git init
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lyc8503/sing-box-rules.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/zuohl/sing-box-rules.git
 git branch -M rule-set-geoip
 git add .
 git commit -m "Update rule-set"
@@ -21,7 +21,7 @@ cd sing-geosite/rule-set
 git init
 git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/lyc8503/sing-box-rules.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/zuohl/sing-box-rules.git
 git branch -M rule-set-geosite
 git add .
 git commit -m "Update rule-set"
